03ede68f4bae5f1e4239794fd35527e2d16c53b9
Kevinzjy <kevinzjy1997@gmail.com>
2023-03-22 19:36:28 +0800

    first release

---
+++ README.md
@@ -0,0 +1,98 @@
+# ManuGPT: rewrite your manuscript using OpenAI GPT model
+
+## About ManuGPT
+
+- Template: bootstrap + sb-admin template + font-awesome
+- Framework: python3 + flask
+- Use OpenAI's [Completions](https://platform.openai.com/docs/api-reference/completions) / [Edits](https://platform.openai.com/docs/api-reference/edits) API to rewrite your paragraph.
+- The prompts for paragraph revision is inspired by https://github.com/manubot/manubot-ai-editor
+
+## Installation
+
+### Prerequisites
+
+- Python 3.x (Tested on Python 3.9.10)
+- Get your own OpenAI API key from https://platform.openai.com/account/api-keys
+- A http proxy is required if you're in countries that can not access OpenAI API directly.
+
+### Clone the repository
+
+```bash
+git clone https://github.com/Kevinzjy/ManuGPT.git
+cd ManuGPT
+```
+
+### Install requirements
+
+```bash
+virtual venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+```
+
+### Configuration
+
+Create the file `~/.openai_key` and save your OpenAI key in it. This is optional because you can also input the API key in the web page.
+
+Replace the following configurations in `app.py`
+
+```python3
+# ============== CONFIG =====================
+
+# IP of flask app host:
+# 127.0.0.1 will only allow local access from your current machine
+# 0.0.0.0 will allow remote access from local network
+FLASK_HOST = "0.0.0.0"
+
+# Port of flask app:
+# Change it to anything you like (should be larger than 1024)
+FLASK_PORT = 47118
+
+# Replace your HTTP proxy host and port here
+# The OpenAI API requires HTTP proxy for access in mainland china
+HTTP_PROXY_HOST = "127.0.0.1"
+HTTP_PROXY_PORT = "1087"
+
+# Set PROXIES to None if you do not need proxy to access openAI
+# PROXIES = None
+PROXIES = {
+    "http": f"http://{HTTP_PROXY_HOST}:{HTTP_PROXY_PORT}",
+    "https": f"http://{HTTP_PROXY_HOST}:{HTTP_PROXY_PORT}",
+}
+
+# Directory for storing temporary files
+TMP_DIR = Path("/tmp")
+
+# ============== CONFIG =====================
+```
+
+### Run ManuGPT
+
+```bash
+python app.py
+```
+
+The ip address will be displayed on your screen if everything works fine.
+
+```bash
+ * Serving Flask app 'app'
+ * Debug mode: off
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on all addresses (0.0.0.0)
+ * Running on http://127.0.0.1:47118
+ * Running on http://192.168.50.40:47118
+Press CTRL+C to quit
+```
+
+## Usage
+
+![image](https://github.com/Kevinzjy/ManuGPT/blob/master/screenshot.png)
+
+1. Input the title / keywords of your manuscript; Select which section (Abstract / Introduction / Results / Discussion / Methods ) your paragraph belongs to.
+2. Select model for text completion (text-davinci-003) or edit (text-davinci-edit-001). The former one is recommended.
+3. Make sure the API key is correct. Otherwise, change it in `app.py` or edit in the webpage.
+4. Input your paragraph is the left text area.
+5. Press the "Submit" button in the upper right. Wait patiently for the response. The revised text will be displayed in the right text area.
+6. Edit the revised paragraph until you're satisfied.
+7. Press the "Download" button to download the docx file containing the raw paragraph and tracked changes.
+
---
+++ app.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+import os
+import re
+import json
+import socket
+import requests
+from pathlib import Path
+import aspose.words as aw
+from datetime import datetime
+from flask import Flask, render_template, request, jsonify, send_file
+
+
+# ============== CONFIG =====================
+
+# IP of flask app host:
+# 127.0.0.1 will only allow local access from your current machine
+# 0.0.0.0 will allow remote access from local network
+FLASK_HOST = "0.0.0.0"
+
+# Port of flask app:
+# Change it to anything you like (should be larger than 1024)
+FLASK_PORT = 47118
+
+# Replace your HTTP proxy host and port here
+# The OpenAI API requires HTTP proxy for access in mainland china
+HTTP_PROXY_HOST = "127.0.0.1"
+HTTP_PROXY_PORT = "1087"
+
+# Set PROXIES to None if you do not need proxy to access openAI
+# PROXIES = None
+PROXIES = {
+    "http": f"http://{HTTP_PROXY_HOST}:{HTTP_PROXY_PORT}",
+    "https": f"http://{HTTP_PROXY_HOST}:{HTTP_PROXY_PORT}",
+}
+
+# Directory for storing temporary files
+TMP_DIR = Path("/tmp")
+
+# ============== CONFIG =====================
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+Several_spaces_pattern = re.compile(r"\s+")
+
+
+@app.route('/')
+def index():
+    # Get hostname
+    hostname = socket.gethostname()
+
+    # Get local ip
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        local_ip = s.getsockname()[0]
+    except Exception as e:
+        local_ip = socket.gethostbyname(hostname + ".local")
+    finally:
+        if s:
+            s.close()
+    api_key = load_api_key()
+
+    return render_template(
+        'index.html', hostname=hostname, local_ip=local_ip, api_key=api_key,
+    )
+
+
+@app.route('/submit-data',  methods=['POST'])
+def submit():
+    form = request.form
+
+    # Set up your OpenAI API key
+    api_key = form['api-key']
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}',
+    }
+
+    if form['input-text'] == "":
+        revised_text = "Please input the paragraph for revision in the left."
+    elif not api_key.startswith("sk-"):
+        revised_text = "Please make sure the OpenAI API key is correct."
+    else:
+        print(f"Waiting for response from openai")
+        revised_text = revise_paragraph(
+            headers, form['title-text'], form['keywords-text'], form['section-text'].lower(),
+            form['input-text'], form['model-text'] == "text-davinci-edit-001",
+        )
+
+    return jsonify({"status": "Success", "revised": revised_text})
+
+
+@app.route('/download-file', methods=['POST', 'GET'])
+def download():
+    form = request.form
+    time_stamp = datetime.now().strftime("%Y%m%d-%H%H%S")
+    revised_file = TMP_DIR / f"ManuGPT.{time_stamp}.docx"
+
+    model_name = 'text-davinci-003' if form['model-text'] == "Completion" else "text-davinci-edit-001"
+
+    print(f"Saving revised docx {revised_file}")
+    save_docx(form['input-text'], form['output-text'], model_name, str(revised_file))
+
+    return send_file(str(revised_file), as_attachment=True)
+
+
+def get_prompt(paragraph_text: str, section_name: str, title: str, keywords: str, edit_endpoint=False):
+    """
+    From: https://github.com/manubot/manubot-ai-editor/blob/main/libs/manubot_ai_editor/models.py
+
+    Returns the prompt to be used for the revision of a paragraph that
+    belongs to a given section. There are three types of prompts according
+    to the section: Abstract, Introduction, Methods, and the rest (i.e.
+    Results, Discussion, etc.).
+    Args:
+        paragraph_text: text of the paragraph to revise.
+        section_name: name of the section the paragraph belongs to.
+    Returns:
+        If self.edit_endpoint is False, then returns a string with the prompt to be used by the model for the revision of the paragraph.
+        It contains two paragraphs of text: the command for the model
+        ("Revise...") and the paragraph to revise.
+        If self.edit_endpoint is True, then returns a tuple with two strings:
+         1) the instructions to be used by the model for the revision of the paragraph,
+         2) the paragraph to revise.
+    """
+    if section_name in ("abstract",):
+        prompt = f"""
+            Revise the following paragraph from the {section_name.capitalize()} of an academic paper (with the title '{title}' and keywords '{keywords}')
+            so the research problem/question is clear,
+               the solution proposed is clear,
+               the text grammar is correct, spelling errors are fixed,
+               and the text is in active voice and has a clear sentence structure
+        """
+    elif section_name in ("introduction", "discussion"):
+        prompt = f"""
+            Revise the following paragraph from the {section_name.capitalize()} section of an academic paper (with the title '{title}' and keywords '{keywords}')
+            so
+               most of the citations to other academic papers are kept,
+               the text minimizes the use of jargon,
+               the text grammar is correct, spelling errors are fixed,
+               and the text has a clear sentence structure
+        """
+    elif section_name in ("results",):
+        prompt = f"""
+            Revise the following paragraph from the {section_name.capitalize()} section of an academic paper (with the title '{title}' and keywords '{keywords}')
+            so
+               most references to figures and tables are kept,
+               the details are enough to clearly explain the outcomes,
+               sentences are concise and to the point,
+               the text minimizes the use of jargon,
+               the text grammar is correct, spelling errors are fixed,
+               and the text has a clear sentence structure
+        """
+    elif section_name in ("methods",):
+        equation_definition = r"$$ ... $$ {#id}"
+        revise_sentence = f"""
+            Revise the paragraph(s) below from
+            the {section_name.capitalize()} section of an academic paper
+            (with the title '{title}' and keywords '{keywords}')
+        """.strip()
+
+        prompt = f"""
+            {revise_sentence}
+            so
+               most of the citations to other academic papers are kept,
+               most of the technical details are kept,
+               most references to equations (such as "Equation (@id)") are kept,
+               all equations definitions (such as '{equation_definition}') are included with newlines before and after,
+               the most important symbols in equations are defined,
+               spelling errors are fixed, the text grammar is correct,
+               and the text has a clear sentence structure
+        """.strip()
+    else:
+        prompt = f"""
+            Revise the following paragraph from the {section_name.capitalize()} section of an academic paper (with the title '{title}' and keywords '{keywords}')
+            so
+               the text minimizes the use of jargon,
+               the text grammar is correct, spelling errors are fixed,
+               and the text has a clear sentence structure
+        """
+
+    prompt = Several_spaces_pattern.sub(" ", prompt).strip()
+
+    if edit_endpoint is False:
+        return f"{prompt}.\n\n{paragraph_text.strip()}"
+    else:
+        prompt = prompt.replace("the following paragraph", "this paragraph")
+        return f"{prompt}.", paragraph_text.strip()
+
+
+def revise_paragraph(headers, title, keywords, section, text, edit_endpoint=False):
+    """
+    Revise your paragraph using openAI API
+    :param headers:
+    :param title:
+    :param keywords:
+    :param section:
+    :param text:
+    :param edit_endpoint:
+    :return:
+    """
+    prompt = get_prompt(text, section, title, keywords, edit_endpoint=edit_endpoint)
+
+    if edit_endpoint is False:
+        json_data = {
+            'model': 'text-davinci-003',
+            'prompt': prompt,
+            'max_tokens': 1024,
+            'temperature': 0.5,
+        }
+        if PROXIES is not None:
+            response = requests.post(
+                'https://api.openai.com/v1/completions',
+                headers=headers,
+                json=json_data,
+                proxies=PROXIES,
+            )
+        else:
+            response = requests.post(
+                'https://api.openai.com/v1/completions',
+                headers=headers,
+                json=json_data,
+            )
+    else:
+        json_data = {
+            'model': 'text-davinci-edit-001',
+            'input': prompt[1],
+            'instruction': prompt[0]
+        }
+        if PROXIES is not None:
+            response = requests.post(
+                'https://api.openai.com/v1/edits',
+                headers=headers,
+                json=json_data,
+                proxies=PROXIES,
+            )
+        else:
+            response = requests.post(
+                'https://api.openai.com/v1/edits',
+                headers=headers,
+                json=json_data,
+            )
+
+    edited = json.loads(response.text.strip())
+    text = edited['choices'][0]['text'].strip()
+
+    return text
+
+
+def save_docx(raw_text, revised_text, model_name, out_file):
+    """
+    Save revised paragraph to docx files and record the changes
+    :param raw_text: str,
+        input text from the user
+    :param revised_text: str,
+        revised text by openAI
+    :param model_name: str,
+        name of model, which will be also used as the author of revised changes in output docx.
+    :param out_file: str,
+        path to output docx
+    """
+    original_doc = aw.Document()
+    builder = aw.DocumentBuilder(original_doc)
+    builder.writeln(raw_text)
+
+    revised_doc = aw.Document()
+    builder = aw.DocumentBuilder(revised_doc)
+    builder.writeln(revised_text)
+
+    original_doc.compare(revised_doc, model_name, datetime.now())
+    original_doc.save(out_file)
+
+
+def load_api_key():
+    key_file = Path.home() / ".openai_key"
+    if key_file.exists():
+        with open(key_file, 'r') as f:
+            return f.readline().rstrip()
+    else:
+        return ""
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host=FLASK_HOST, port=FLASK_PORT)
